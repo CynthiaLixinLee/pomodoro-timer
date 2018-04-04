cb8a5cbe9a875ffaa7a305b8ec06eebc9e2f1d92
Cynthia <29832823+CynthiaLixinLee@users.noreply.github.com>
2018-04-04 11:31:06 -0400

    refactored js

--- js/index.js
+++ js/index.js
@@ -1,16 +1,21 @@
-let countdown = 0;
-let seconds = 0;
-let remaining = 0;
+let countdown = 0; // variable to set/clear intervals
+let seconds = 1500; // seconds left on the clock
+let pauseTime = 0; // seconds left when the timer is paused
 let isBreak = true;
 let isPaused = true;
+let statusText;
+
 const status = document.querySelector("#status");
 const timerDisplay = document.querySelector(".timerDisplay");
 const startBtn = document.querySelector("#start-btn");
 const resetBtn = document.querySelector("#reset");
 const workMin = document.querySelector("#work-min");
 const breakMin = document.querySelector("#break-min");
-const alarm = document.createElement('audio');
+
+const workingText = "Keep Working";
+const alarm = document.createElement('audio'); // A bell sound will play when the timer reaches 0
 alarm.setAttribute("src", "https://www.soundjay.com/misc/sounds/bell-ringing-05.mp3");
+
 
 /* EVENT LISTENERS FOR START AND RESET BUTTONS */
 startBtn.addEventListener('click', () => {
@@ -20,7 +25,7 @@ startBtn.addEventListener('click', () => {
     startBtn.textContent = "Pause";
     isPaused = false;
   } else {
-    remaining = seconds;
+    pauseTime = seconds;
     startBtn.textContent = "Continue";
     isPaused = true;
   }
@@ -30,15 +35,14 @@ resetBtn.addEventListener('click', () => {
   clearInterval(countdown);
   seconds = workMin.textContent * 60;
   countdown = 0;
-  remaining = 0;
+  pauseTime = 0;
   isPaused = true;
   isBreak = true;
-  status.textContent = "Keep Working";
+  statusText = "";
   startBtn.textContent = "Start";
-  displayTimeLeft(seconds);
 })
 
-/* MAIN FUNCTIONS - TIMER, START COUNTDOWN, & UPDATE DISPLAY */
+/* MAIN FUNCTIONS - TIMER, START COUNTDOWN */
 function timer() {
   seconds --;
   if (seconds < 0) {
@@ -48,65 +52,52 @@ function timer() {
 
     if (isBreak) {
       seconds = breakMin.textContent * 60;
-      status.textContent = "Take a Break!";
+      statusText = "Take a Break!";
       isBreak = false;
     } else {
       seconds = workMin.textContent * 60;
-      status.textContent = "Keep Working";
+      statusText = workingText;
       isBreak = true;
     }
     countdown = setInterval(timer, 1000);
     return;
   }
-  displayTimeLeft(seconds); // Keep updating display
 }
 
 function startCountdown() {
-  if (remaining != 0) {
-    seconds = remaining;
+  if (pauseTime != 0) {
+    seconds = pauseTime;
   } else {
     seconds = workMin.textContent * 60;
-    status.textContent = "Keep Working";
+    statusText = workingText;
   }
   countdown = setInterval(timer, 1000);
 }
 
-function displayTimeLeft(seconds) {
-  const minutes = Math.floor(seconds / 60);
-  const remainderSeconds = seconds % 60;
-  timerDisplay.textContent = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+/* UPDATE WORK AND BREAK TIMES */
+let x = parseInt(workMin.textContent);
+let y = parseInt(breakMin.textContent);
+
+document.querySelector("#work-plus").onclick = function() {
+  x < 60 ? x+=5 : x;
+}
+document.querySelector("#work-minus").onclick = function() {
+  x > 5 ? x-=5 : x;
+}
+document.querySelector("#break-plus").onclick = function() {
+  y < 60 ? y+=5 : y;
+}
+document.querySelector("#break-minus").onclick = function() {
+  y > 5 ? y-=5 : y;
 }
 
-/* UPDATE WORK AND BREAK TIMES */
-const workPlus = document.querySelector("#work-plus");
-const workMinus = document.querySelector("#work-minus");
-const breakPlus = document.querySelector("#break-plus");
-const breakMinus = document.querySelector("#break-minus");
-
-workPlus.addEventListener('click', () => {
-  let x = parseInt(workMin.textContent);          
-  if (x < 60) {
-    workMin.textContent = x+5;
-  }                       
-})
-
-workMinus.addEventListener('click', () => {
-  let x = parseInt(workMin.textContent);          
-  if (x > 5) {
-    workMin.textContent = x-5;
-  }                       
-})
-
-breakPlus.addEventListener('click', () => {
-  let x = parseInt(breakMin.textContent);          
-  if (x < 60) {
-    breakMin.textContent = x+5;
-  }                       
-})
-
-breakMinus.addEventListener('click', () => {
-  let x = parseInt(breakMin.textContent);          
-  if (x > 5) {
-    breakMin.textContent = x-5;
-  }                       
-})
+/* RUNS 10X PER SECOND TO UPDATE HTML CONTENT */
+window.setInterval(function() {
+  workMin.textContent = x;
+  breakMin.textContent = y;
+  status.textContent = statusText;
+  // Updates countdown display
+  let minutes = Math.floor(seconds / 60);
+  let remainderSeconds = seconds % 60;
+  timerDisplay.textContent = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+}, 100);
