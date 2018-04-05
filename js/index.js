3e14c72e8a576fab1a5a88ed8ea87bc6be98d2b4
Cynthia <29832823+CynthiaLixinLee@users.noreply.github.com>
2018-04-04 21:19:18 -0400

    refactored code

--- js/index.js
+++ js/index.js
@@ -1,9 +1,7 @@
 let countdown = 0; // variable to set/clear intervals
 let seconds = 1500; // seconds left on the clock
-let pauseTime = 0; // seconds left when the timer is paused
 let isBreak = true;
 let isPaused = true;
-let statusText;
 
 const status = document.querySelector("#status");
 const timerDisplay = document.querySelector(".timerDisplay");
@@ -12,7 +10,9 @@ const resetBtn = document.querySelector("#reset");
 const workMin = document.querySelector("#work-min");
 const breakMin = document.querySelector("#break-min");
 
-const workingText = "Keep Working";
+let x = parseInt(workMin.textContent);  // Work duration
+let y = parseInt(breakMin.textContent);  // Break duration
+
 const alarm = document.createElement('audio'); // A bell sound will play when the timer reaches 0
 alarm.setAttribute("src", "https://www.soundjay.com/misc/sounds/bell-ringing-05.mp3");
 
@@ -21,28 +21,22 @@ alarm.setAttribute("src", "https://www.soundjay.com/misc/sounds/bell-ringing-05.
 startBtn.addEventListener('click', () => {
   clearInterval(countdown);
   if (isPaused) {
-    startCountdown();
-    startBtn.textContent = "Pause";
+    countdown = setInterval(timer, 1000);
     isPaused = false;
   } else {
-    pauseTime = seconds;
-    startBtn.textContent = "Continue";
     isPaused = true;
   }
 })
 
 resetBtn.addEventListener('click', () => {
   clearInterval(countdown);
-  seconds = workMin.textContent * 60;
+  seconds = x * 60;
   countdown = 0;
-  pauseTime = 0;
   isPaused = true;
   isBreak = true;
-  statusText = "";
-  startBtn.textContent = "Start";
 })
 
-/* MAIN FUNCTIONS - TIMER, START COUNTDOWN */
+/* TIMER - HANDLES COUNTDOWN */
 function timer() {
   seconds --;
   if (seconds < 0) {
@@ -51,33 +45,17 @@ function timer() {
     alarm.play();
 
     if (isBreak) {
-      seconds = breakMin.textContent * 60;
-      statusText = "Take a Break!";
+      seconds = y * 60;
       isBreak = false;
     } else {
-      seconds = workMin.textContent * 60;
-      statusText = workingText;
+      seconds = x * 60;
       isBreak = true;
     }
-    countdown = setInterval(timer, 1000);
-    return;
   }
 }
 
-function startCountdown() {
-  if (pauseTime != 0) {
-    seconds = pauseTime;
-  } else {
-    seconds = workMin.textContent * 60;
-    statusText = workingText;
-  }
-  countdown = setInterval(timer, 1000);
-}
 
 /* UPDATE WORK AND BREAK TIMES */
-let x = parseInt(workMin.textContent);
-let y = parseInt(breakMin.textContent);
-
 document.querySelector("#work-plus").onclick = function() {
   x < 60 ? x+=5 : x;
 }
@@ -93,9 +71,19 @@ document.querySelector("#break-minus").onclick = function() {
 
 /* RUNS 10X PER SECOND TO UPDATE HTML CONTENT */
 window.setInterval(function() {
+  isBreak ? status.textContent = "Keep Working" : status.textContent = "Take a Break!";
+
+  if (isPaused && countdown === 0) {
+    startBtn.textContent = "START";
+  } else if (isPaused && countdown !== 0) {
+    startBtn.textContent = "Continue";
+  } else {
+    startBtn.textContent = "Pause";
+  }
+
   workMin.textContent = x;
   breakMin.textContent = y;
-  status.textContent = statusText;
+
   // Updates countdown display
   let minutes = Math.floor(seconds / 60);
   let remainderSeconds = seconds % 60;
